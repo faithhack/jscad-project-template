970b1a2e8e262cf8cf9b3e12916818ffdafb154a
Chris Jackson <chris@faithhack.com>
2020-01-26 10:23:32 -0500

    Adding more complete list of useful functions

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # jscad-project-template
 A very simple layout I use with OpenJSCAD V2 when creating 3D printing designs.
-This currently depends on https://github.com/jscad/OpenJSCAD.org.git V2 branch with the v2-3d-hull merged in.
+This currently depends on https://github.com/jscad/OpenJSCAD.org.git V2 branch.
 The fonts directory is from https://github.com/lautr3k/jscad-vector-fonts with additional single-line fonts referenced 
 at http://imajeenyus.com/computer/20150110_single_line_fonts/index.shtml
+
+To make use of this template with the OpenJSCAD web app:
+* install and run OpenJSCAD from localhost
+* clone this project
+* edit the project.json as needed, the 'name' attribute will be used by OpenJSCAD to as the default name for rendered parts
+
+You can change which part is rendered by changing file name in the first 'require' in index.js:
+`var inc = require('./my_other_part.js')`
--- mypart.js
+++ mypart.js
@@ -1,15 +1,43 @@
 /* All the csg API functions: */
-/* All the csg API functions: */
 const modeling = require('@jscad/modeling')
-const { color, colorManeToRgb, cssColors, hexToRgb, hslToRgb, hsvToRgb, hueRoColorComponant, rgbToHex, rgbToHsl, rgbToHsv } = modeling.color
-const { create, fromPointAxisNormal, transformationBetween } = modeling.connectors
+const { color, colorManeToRgb, cssColors, hexToRgb, hslToRgb, hsvToRgb, hueToColorComponant, rgbToHex, rgbToHsl, rgbToHsv } = modeling.color
+const { create, fromPointAxisNormal, transformationBetween } = modeling.connectors // extends, normalize?
+const connectorTransform = modeling.connectors.transform // so as not to overlap with modeling.transforms.transform
 const { geom2, geom3, path2, poly2, poly3 } = modeling.geometry
+// geom2   clone,create,fromPoints,isA,reverse,toOutlines,toPoints,toSides,toString,transform
+// geom3   clone,create,fromPoints,isA,toPoints,toPolygons,toString,transform
+// path2   appendArc,appendBezier,appendPoints,clone,close,concat,create,eachPoint,equals,fromPoints,
+//         isA,reverse,toPoints,toString,transform
+// poly2   arePointsInside:,create,flip,measureArea
+// poly3   clone,create,flip,fromPoints,fromPointsAndPlane,isA,isConvex,measureArea,measureBoundingBox,
+//         measureBoundingSphere,measureSignedVolume,toPoints,toString,transform
 const { constants, line2, line3, mat4, plane, utils, vec2, vec3, vec4 } = modeling.math
-const { arc, circle, cube, cuboid, cylinder, cylinderElliptic, ellipse, ellipsoid, geodesicSphere, line, polygon, polyhedron, rectangle, roundedCuboid, roundedCylinder, roundedRectangle, sphere, square, star, torus } = require('@jscad/modeling').primitives
+// line2   clone,closestPoint,create,direction,distanceToPoint,equals,fromPoints,fromValues,
+//         intersectPointOfLines,origin,reverse,toString,transform,xAtY
+// line3   clone,closestPoint,create,direction,distanceToPoint,equals,fromPlanes,fromPointAndDirection,
+//         fromPoints,intersectPointOfLineAndPlane,origin,reverse,toString,transform
+// mat4    add,clone,create,equals,fromRotation,fromScaling,fromTaitBryanRotation,fromTranslation,
+//         fromValues,fromXRotation,fromYRotation,fromZRotation,identity,isMirroring,
+//         mirror,mirrorByPlane,multiply,rightMultiplyVec2,rightMultiplyVec3,rotate,rotateX,rotateY,rotateZ
+//         scale,subtract,toString,translate
+// plane   clone,create,equals,flip,fromNormalAndPoint,fromValues,fromPoints,fromPointsRandom,
+//         signedDistanceToPoint,splitLineSegmentByPlane,toString,transform
+// utils   area,clamp,degToRad,intersect,radToDeg,solve2Linear
+// vec2    abs,add,angle,angleDegrees,angleRadians,canonicalize,clone,create,cross,distance,divide,
+//         dot,equals,fromAngle,fromAngleDegrees,fromAngleRadians,fromArray,fromScalar,fromValues,length,
+//         lerp,max,min,multiply,negate,normal,normalize,rotate,scale,squaredDistance,squaredLength,
+//         subtract,toString,transform
+// vec3    abs,add,angle,canonicalize,clone,create,cross,distance,divide,dot,equals,fromArray,fromScalar,
+//         fromValues,fromVec2,length,lerp,max,min,multiply,negate,normalize,random,
+//         rotateX,rotateY,rotateZ,scale,squaredDistance,squaredLength,subtract,toString,transform,unit
+// vec4    clone,create,fromScalar,fromValues,toString,transform
+const { degToRad, radToDeg } = modeling.math.utils
+const { arc, circle, cube, cuboid, cylinder, cylinderElliptic, ellipse, ellipsoid, geodesicSphere, 
+		line, polygon, polyhedron, rectangle, roundedCuboid, roundedCylinder, roundedRectangle, 
+		sphere, square, star, torus } = require('@jscad/modeling').primitives
 const { vectorChar, vectorText } = modeling.text
 const {areAllShapesTheSameType, flatten, fnNumberSort, insertSorted, interpolateBetween2DPointsForY } = modeling.utils
-
-const { intersect, subtract, union } = modeling.booleans
+const { intersect, subtract, union } = modeling.booleans // subtract = difference ?
 const { expand, offset } = modeling.expansions
 const {extrudeFromSlices, extrudeLinear, extrudeRectangular, extrudeRotate, slice} = modeling.extrusions
 const { hull, hullChain } = modeling.hulls
